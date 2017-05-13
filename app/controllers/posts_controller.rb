9af79589b200d8e68793a310c53d466aa33f6758
Chris White <=>
2017-05-13 13:07:47 +0000

    Edit post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,4 +21,14 @@ class PostsController < ApplicationController
     def show
         @post = Post.find(params[:id])
     end
+    
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    def update  
+       @post = Post.find(params[:id])
+       @post.update(post_params)
+       redirect_to(post_path(@post))
+    end  
 end
