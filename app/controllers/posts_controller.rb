a6280b281bf8a6bc96b1f80e9340179001dc67c1
Chris White <=>
2017-05-13 13:26:27 +0000

    Post id method created and cleaned up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,40 +1,43 @@
 class PostsController < ApplicationController
-
-    private def post_params
-        params.require(:post).permit(:image, :caption)
+  before_action :set_post, only:[:show,:edit,:update,:destroy]
+  
+  private def post_params
+    params.require(:post).permit(:image, :caption)
+  end
+  
+  private def set_post
+    @post = Post.find(params[:id])
+  end
+  
+  def index
+    @posts = Post.all
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    if @post = Post.create(post_params)
+      redirect_to posts_path
+    else
+      render :new
     end
-    
-    def index
-        @posts = Post.all
-    end
-    
-    def new
-       @post = Post.new
-    end
-    
-    def create
-        @post = Post.new(post_params)
-        @post.save
-        redirect_to posts_path
-    end
-    
-    def show
-        @post = Post.find(params[:id])
-    end
-    
-    def edit
-        @post = Post.find(params[:id])
-    end
-    
-    def update  
-       @post = Post.find(params[:id])
-       @post.update(post_params)
-       redirect_to(post_path(@post))
-    end  
-    
-    def destroy
-        @post = Post.find(params[:id])
-        @post.destroy
-        redirect_to posts_path
-    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update  
+   @post.update(post_params)
+   redirect_to(post_path(@post))
+  end  
+  
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
 end
